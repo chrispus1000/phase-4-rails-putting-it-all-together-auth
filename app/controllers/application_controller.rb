0b76e25ad36ad41f9825a1d235a2bd49fda01f3d
chrispus1000 <minrevac@gmail.com>
2022-10-06 02:06:01 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
+  private
+
+  def authorize
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+  end
+
+  def render_unprocessable_entity_response(e)
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
